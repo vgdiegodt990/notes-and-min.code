5aff2492400cfb160ae7ece1cafedd16dbca54c9
vgdiegodt990 <47872862+vgdiegodt990@users.noreply.github.com>
2022-05-09 23:58:12 -0400

    Add files via upload

---
+++ CSharpPractice/GettingStarted/GettingStarted/Program.cs
@@ -0,0 +1,171 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Variables;
+using UserInputs;
+using Conv;
+using Array;
+using Casting;
+using Constants; 
+
+//Initial Start
+
+namespace Hello
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+            VarTest.Second();
+            UserData.Inputs();
+            Conversion.Converting();
+            TestArray.NumArray();
+            TestCasting.ImpExp();
+            ConstantTest.ConstEnum(); 
+            Console.WriteLine("Press Any Key To Continue...");
+            Console.ReadKey();
+        }
+    }
+}
+
+//Variables 
+
+namespace Variables
+{
+    public class VarTest
+    {
+        public static void Second()
+        {
+            Console.Title = "Variables";
+            //Declaring then initializing
+            char letter; letter = 'A';
+            int number; number = 100;
+            float body = 98.6f;
+            double pi = 3.14159;
+            decimal sum = 1000.00m;
+            bool flag = false;
+            string text = "C# Is... Interesting";
+            //In use
+            Console.WriteLine("char letter:\t" + letter);
+            Console.WriteLine("int number:\t" + number);
+            Console.WriteLine("float body:\t" + body);
+            Console.WriteLine("double pi:\t" + pi);
+            Console.WriteLine("decimal sum:\t" + sum);
+            Console.WriteLine("bool flag:\t" + flag);
+            Console.WriteLine("string text:\t" + text);
+        }
+    }
+} 
+ 
+namespace UserInputs
+{
+#pragma warning disable CS8600
+    public class UserData
+    {
+        public static void Inputs()
+        {
+            Console.Title = "Test to see if this'll override";
+            Console.Write("Please Enter Your Name:");
+            string name = Console.ReadLine();
+            Console.WriteLine("Welcome " + name + "!");
+        }
+    }
+#pragma warning restore CS8600
+} 
+
+namespace Conv
+{
+    public class Conversion
+    {
+        public static void Converting()
+        {
+            Console.Write("Please Enter A Number:");
+            double num = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Now Enter Another Number:");
+            double sum =
+                num + Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine("Total = " + sum);
+        }
+    }
+}
+
+namespace Array
+{
+    public class TestArray
+    {
+        public static void NumArray()
+        {
+            string[] cars = new string[3]
+                { "BMW", "Ford", "Opel" };
+            int[,] coords = new int[2, 3]
+                { {1,2,3},{4,5,6} };
+            Console.WriteLine("Second Car: " + cars[1]);
+            Console.WriteLine("X1, XY2: " + coords[0, 0]);
+            Console.WriteLine("X2, Y3: " + coords[1, 2]);
+        }
+    }
+}
+
+namespace Casting
+{
+    public class TestCasting
+    {
+        public static void ImpExp()
+        {
+            double impsum = (double)7 / 2; //Stores accurate floating-point results b/c cast into suitable data type 
+            double nomsum = 7 / 2; //Not casted in suitable data type thus does not store accurate floating-point 
+            // the 7/2 is a truncated integer, needs to be cast as a double, implicit 
+            Console.WriteLine(impsum + " || Accurate floating point, was casted");
+            Console.WriteLine(nomsum + " || Non-accurate floating point, was NOT casted");
+            //Testing operator casting precendence 
+            double prenum = (double)7 / 2.5; //2.5 is seen w/ it's floating point
+            Console.WriteLine(prenum + " || Testing operator precendence");
+            //int prenum = (int)7/2.5 would not work b/c the 2.5 int does not support floating points 
+            //more exaples
+            double num = 10.5;
+            int integer = 2;
+            //implicity casting the integer value into a double-precision value
+            num = num + integer;
+            Console.WriteLine("Implicit Cast: " + num);
+            num = (double)7 / integer;
+            //explicitly casting the result of an integer division into a double-precision value 
+            Console.WriteLine("Explicit Cast: " + num);
+            char letter = (char)65;
+            //casting integer value into a char data type to display the ASCII equivalent character
+            Console.WriteLine("Cast Integer: " + letter);
+            int ascii = (int)'A';
+            Console.WriteLine("Cast Letter: " + ascii);
+            char symbol = (char)102;
+            Console.WriteLine("Cast Symol " + symbol);
+        }
+    }
+}
+
+namespace Constants
+{
+    public static class ConstantTest
+    {
+        enum Days { Sat, Sun, Mon, Tue, Wed, Thu, Fri };
+        public static void ConstEnum()
+        {
+            const double pi = 3.14158265358969;
+            var daysType = typeof(Days);
+            //revealing constant's data type and output value
+            Console.WriteLine("Pi Type: " + pi.GetType() );
+            Console.WriteLine("Circumference " + (pi * 3 ) );
+            //adding statements to display the name and index position of the first item in the enumerator list 
+            Console.WriteLine("\nFirst Name: " + Days.Sat);
+            Console.WriteLine("1st Index " + (int)Days.Sat);
+            //statements to display the name at the second index position and querying the enumerator list 
+            string name = Enum.GetName(daysType, 1);
+            Console.WriteLine("\n2nd Index: " + name);
+            bool flag = Enum.IsDefined(daysType, "Mon");
+            Console.WriteLine("Contains Mon?: " + flag);
+        }
+        
+    }
+}
---
+++ CSharpPractice/GettingStarted2/GettingStarted2/Program.cs
@@ -0,0 +1,164 @@
+﻿// See https://aka.ms/new-console-template for more information
+/* Testing new format 
+Console.WriteLine("Hello, World!");
+string newFormat = "Testing the new format.";
+Console.WriteLine(newFormat);
+Testing.ContinualTest.FirstTest(); 
+Console.ReadKey(); 
+
+namespace Testing
+{
+    public class ContinualTest
+    {
+        public static void FirstTest()
+        {
+            string namesTest = "Testing if namespaces work.";
+            Console.WriteLine(namesTest); 
+        }
+    }
+} */
+
+Console.WriteLine("Starting line.");
+Arithmetic.Basics.ArithExamples();
+Arithmetic.Basics.AssignValues();
+Arithmetic.Basics.CompValues();
+Arithmetic.Basics.AssessLogic();
+Arithmetic.Basics.TernaryOp();
+Arithmetic.Basics.OpPrecedence();
+//Arithmetic.Basics.minorTest();
+Console.ReadKey();
+
+namespace Arithmetic
+{
+    public class Basics
+    {
+        public static void ArithExamples()
+        {
+            int a = 8;
+            int b = 4;
+            Console.WriteLine("Addition:\t " + (a + b));
+            Console.WriteLine("Subtraction:\t " + (a - b));
+            Console.WriteLine("Multiplication:\t " + (a * b));
+            Console.WriteLine("Division:\t " + (a / b));
+            Console.WriteLine("Modulus:\t " + (a % b));
+
+            //Postfix Result
+            Console.WriteLine("\nPostfix:\t " + (a++));
+            Console.WriteLine("Postfix Result:\t " + a);
+
+            //Prefix Result  
+            Console.WriteLine("\nPrefix:\t " + (++b));
+            Console.WriteLine("Prefix Result:\t " + b);
+        }
+        
+        public static void AssignValues()
+        {
+            //Declaring then assigning 
+            int a, b;
+            Console.Write("Assign Values: ");
+            Console.Write("\t a = " + (a = 8));
+            Console.WriteLine("\t b = " + (b = 4));
+            //Adding statements 
+            Console.Write("\n\nAdd and Assign: ");
+            Console.Write("\t a += b (8 += 4)\t a = " + (a += b));
+
+            Console.Write("\nSubtract and Assign: ");
+            Console.Write("\t a-= b (8 -= 4)\t\t a = " + (a -= b));
+
+            Console.Write("\n\nMultiply and Assign: ");
+            Console.Write("\t a *= b (8 *= 4)\t a = " + (a *= b));
+
+            Console.Write("\n\nDivide and Assign: ");
+            Console.Write("\t a /= b (8 /= 4)\t a = " + (a /= b));
+
+            Console.Write("\n\nModulus and Assign: ");
+            Console.Write("\t a %= b (8 %= b)\t a = " + (a %= b));
+        }
+
+        public static void CompValues()
+        {
+            //Declaring
+            int nil = 0, num = 0, max = 1;
+            char cap = 'A', low = 'a';
+            //Equality comparison statements 
+            Console.Write("\n\nEquality");
+            Console.Write("\n(0 == 0): " + (nil == num));
+            Console.Write("\n(A == a): " + (cap == low));
+            //Other integer comparisons 
+            Console.Write("\nInequality:");
+            Console.Write("\t(0 != 1):" + (nil != max));
+
+            Console.Write("\nGreater:");
+            Console.Write("\t(0 > 1): " + (nil > max));
+
+            Console.Write("\nLess: ");
+            Console.Write("\t\t(0 < 1): " + (nil < max));
+
+            Console.Write("\nGreater/Equal: ");
+            Console.Write("\t(0 >= 0): " + (nil >= num));
+
+            Console.Write("\nLess or Equal: ");
+            Console.Write("\t(1 <= 0): " + (max <= nil));
+        }
+
+        public static void AssessLogic()
+        {
+            //Declare
+            bool yes = true, no = false;
+            //AND Logic
+            Console.Write("\n\n\tAND Logic:");
+            Console.Write("\n\t(yes && yes): " + (yes && yes)); //Expected: True
+            Console.Write("\n\t(yes && no): " + (yes && no)); //False
+            Console.Write("\n\t(no && no): " + (no && no)); //False
+
+            //OR Logic
+            Console.Write("\n\t(yes || yes): " + (yes || yes)); //True
+            Console.Write("\n\t(yes || no): " + (yes || no)); //True
+            Console.Write("\n\t(no || no): " + (no || no)); //False
+
+            //NOT Logic 
+            Console.Write("\n\t yes = " + yes); //true
+            Console.Write("\n\t yes != " + !yes); //false
+            Console.Write("\n\t no = " + no); //false
+            Console.Write("\n\t no != " + !no); //true
+        }
+
+        public static void TernaryOp()
+        {
+            int a = 8, b = 3;
+            string verb = (a != 1) ? " are " : " is ";
+            Console.Write("\n\nThere" + verb + a + ".\n");
+
+            string parity = (a % 2 != 0) ? "Odd" : "Even";
+            Console.Write(a + " is " + parity + ".\n");
+            parity = (b % 2 != 0) ? "Odd" : "Even";
+            Console.Write(b + " is " + parity + ".\n");
+
+            int max = (a > b) ? a : b;
+            Console.Write("Maximum is " + max + ".");
+        }
+
+        public static void OpPrecedence()
+        {
+            int sum;
+            sum = 1 + 4 * 3;
+            Console.Write("\n\nDefault Order:\t\t" + sum); //15 (wrong, was 13 b/c of * multiplication operator precedence) 
+            sum = (1 + 4) * 3; 
+            Console.Write("\nForced Order:\t\t" + sum); //15
+            sum = 7 - 4 + 2;
+            Console.Write("\nDefault Direction:\t" + sum); //5
+            sum = 7 - (4 + 2);
+            Console.Write("\nForced Direction:\t" + sum); //1
+        }
+
+        /*
+        public static void minorTest()
+        {
+            int sum = 1;
+            Console.WriteLine(sum);
+            int sum = 2; //already defined, test done -- was testing difference between defining off the bat vs previous excercise which declared int then used it as a shell
+            Console.WriteLine(sum);
+        }
+        */
+    }
+}
